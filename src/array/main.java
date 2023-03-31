8406da22e8ccd328f2a39356797b31d470d272da
Diego Poles <poles.diego@itiscannizzaro.edu.it>
2023-03-31 12:27:05 +0200

    Esercitazione su Frame e Arrey/BAckground Color

--- src/array/main.java
+++ src/array/main.java
@@ -2,7 +2,7 @@ package array;
 import javax.swing.*;
 public class main {
     public static void main(String[]args){
-        frame_array ModuloRegistrazione = new frame_array();
+        framearray ModuloRegistrazione = new framearray();
     }
 }
 
--- src/color/main.java
+++ src/color/main.java
@@ -2,6 +2,6 @@ package color;
 
 public class main {
 	public static void main(String[]args){
-        frame_colore SceltaColore = new frame_colore();
+        framecolore SceltaColore = new framecolore();
     }
 }
